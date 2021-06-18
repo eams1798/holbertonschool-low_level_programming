720e138ea79a120eb0a8be7c38a22dc27e086871
estefanomisme <3401@holbertonschool.com>
2021-06-18 08:07:01 +0000

    uploading advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*Print a sentence without using printf and puts either*/
+
+int main(void)
+{
+	echo("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
