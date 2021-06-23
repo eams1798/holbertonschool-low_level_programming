f30bf48f9fca154c5a0defd33f23072bd24a0dda
estefanomisme <3401@holbertonschool.com>
2021-06-23 08:19:05 +0000

    adding holberton library

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-/**
- * print_alphabet - print the lowercase alphabet
- *
- * Return: the lowercase alphabet
- */
-void print_alphabet(void)
-{
-	char alph;
+#include "holberton.h"
 
-	for (alph = 'a'; alph <= 'z'; alph++)
-	{
-		putchar(alph);
-	}
-	putchar('\n');
+void print_alphabet(void);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
