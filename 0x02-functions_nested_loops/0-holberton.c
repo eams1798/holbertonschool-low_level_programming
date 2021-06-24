fbe436d6e8a19e196a8edc81fb0f0035448342c4
estefanomisme <3401@holbertonschool.com>
2021-06-24 05:18:34 +0000

    validating function #0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,18 +4,19 @@
  * main - Print the word "Holberton"
  *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
-	char h[] = "Holberton";
-	int n = sizeof(h);
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		_putchar(h[i]);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
