adc105fe348563b64d84c501874efb229cd97134
estefanomisme <3401@holbertonschool.com>
2021-07-14 05:17:11 +0000

    task #2 to concatenate strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: a pointer to a newly allocated space in memory which contains the
+ * contents of the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	size_t lens1 = 0, lens2 = 0, lenstr, i;
+	char *str;
+
+	if (s2 == NULL)
+		return (s1);
+	while (s1[lens1] != '\0')
+		lens1++;
+	while (s2[lens2] != '\0')
+		lens2++;
+	lens2++;
+	str = (char *)malloc((lens1 + lens2) * sizeof(char));
+	if (s1 == NULL)
+		return (NULL);
+	lenstr = lens1 + lens2;
+	for (i = 0; i < lens1; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (i = 0; i < lens2; i++)
+	{
+		str[i + lens1] = s2[i];
+	}
+	return (str);
+}
