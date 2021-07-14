294f621cda8358bcfc4d53a3f1a8905cb2f92a79
estefanomisme <3401@holbertonschool.com>
2021-07-14 04:34:39 +0000

    adding file #1 - a function to copy strings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+
+/**
+ * _strdup - duplicates a string
+ * @str: a string to be duplicated
+ *
+ * Return: pointer to a newly allocated space in memory, which contains a
+ * copy of the string given as a parameter, or NULL if there is an error
+ */
+char *_strdup(char *str)
+{
+	size_t n = 0, i = 0;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[n] != '\0')
+		n++;
+	n++;
+	dup = (char *)malloc(n * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+	while (i < n)
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	return (dup);
+}
