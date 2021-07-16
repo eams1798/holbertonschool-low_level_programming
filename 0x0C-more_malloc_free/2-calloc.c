471d06d77100e6e640d536fae2c56ca2f0cddb32
estefanomisme <3401@holbertonschool.com>
2021-07-16 02:29:33 +0000

    task #2 - calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array and fills its elements with zero
+ * @nmemb: number of elements of the array
+ * @size: the size of the type
+ *
+ * Return: a pointer to the allocated memory or NULL if the function fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *cll;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	cll = malloc(nmemb * size);
+	if (cll == NULL)
+	{
+		free(cll);
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+		cll[i] = 48 - '0';
+	return (cll);
+}
