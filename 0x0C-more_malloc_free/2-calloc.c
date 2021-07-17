39c6d962012fe321db4aef71b7a3bba0de2e6295
estefanomisme <3401@holbertonschool.com>
2021-07-17 03:04:57 +0000

    task #2 - not a real fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		free(cll);
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		cll[i] = 48 - '0';
 	return (cll);
 }
