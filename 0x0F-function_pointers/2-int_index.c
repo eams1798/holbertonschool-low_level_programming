39cf2b3017d872b85e5cb1fc1edc9e42fcb7f40f
estefanomisme <3401@holbertonschool.com>
2021-07-22 03:26:39 +0000

    fixing return value in task #2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 /*
  * int_index - searches for an integer based on some given conditions
  * @array: an array to be analyzed
@@ -10,9 +12,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (size <= 0 || array == NULL)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
 	}
+	return (-1);
 }
