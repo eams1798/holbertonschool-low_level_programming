68822cee2a727ac87aa01214cb379ea61a0dbe61
estefanomisme <3401@holbertonschool.com>
2021-08-06 06:19:26 +0000

    bitwise operators - fixing errors in tasks #0 and adding task #5

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <math.h>
 
 /**
  * binary_to_uint - converts a binary number to an unsigned integer
@@ -9,7 +8,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0, expb = 0, b2, pow10, pow2;
+	int i = 0, expb = 0, b2, j, i2, pow10 = 1, pow2;
 	unsigned int bin, mult, dec = 0;
 
 	while (b[i] != '\0')
@@ -20,14 +19,17 @@ unsigned int binary_to_uint(const char *b)
 	}
 	bin = atoi(b);
 	i--;
+	i2 = i;
 	for (; i >= 0; i--)
 	{
-		pow10 = pow(10, expb);
-		pow2 = pow(2, expb);
+		for (j = 0; j < i2 - i; j++)
+			pow10 *= 10;
+		pow2 = 1 << expb;
 		b2 = (bin / pow10) % 10;
 		mult = b2 * pow2;
 		dec += mult;
 		expb++;
+		pow10 = 1;
 	}
 	return (dec);
 }
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: the number to extract its index
+ * @index: the index of the given number
+ *
+ * Return: the value of a bit at a given index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((n >> index) & 1);
+}
+
+/**
+ * store_bin - stores a binary number in a string
+ * @n: the number to convert to binary and store in an string
+ * @buffer: a buffer for the string
+ *
+ * Return: a string with the binary number
+ */
+char *store_bin(unsigned long int n, int buffer)
+{
+	char *str = malloc(buffer * sizeof(char));
+	int i;
+
+	if (str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+	for (i = buffer - 2; i >= 0; i--)
+		str[i] = (get_bit(n, buffer - i - 2) + '0');
+	str[buffer - 1] = '\0';
+	return (str);
+}
+
+/**
+ * flip_bits - returns the number of bits you would need to flip to get from
+ * one number to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: the number of bits you would need to flip to get from one number
+ * to another
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	char *strN, *strM;
+	int buffer, cn = 1, cm = 1, flipcount = 0;
+	int auxN = n, auxM = m;
+
+	while (auxN > 1)
+	{
+		auxN /= 2;
+		cn++;
+	}
+	while (auxM > 1)
+	{
+		auxM /= 2;
+		cm++;
+	}
+	if (cn >= cm)
+		buffer = cn;
+	else
+		buffer = cm;
+	buffer++;
+	strN = store_bin(n, buffer);
+	strM = store_bin(m, buffer);
+	if (strN == NULL || strM == NULL)
+	{
+		free(strN);
+		free(strM);
+		return (-1);
+	}
+	buffer--;
+	while (*strN != '\0')
+	{
+		if (*strN != *strM)
+			flipcount++;
+		strN++;
+		strM++;
+	}
+	strN -= buffer;
+	strM -= buffer;
+	free(strN);
+	free(strM);
+	return (flipcount);
+}
