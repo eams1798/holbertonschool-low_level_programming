ffae93311daac9ad9593fd12401cf243e96eb444
estefanomisme <3401@holbertonschool.com>
2021-08-10 04:38:33 +0000

    Truncating write process if the file exists

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fl = open(filename, (O_EXCL | O_CREAT | O_WRONLY));
+	fl = open(filename, (O_EXCL | O_CREAT | O_TRUNC | O_WRONLY));
 	if (fl == -1)
 		return (-1);
 	if (text_content != NULL)
