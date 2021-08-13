42c79640a02a5fd6619f6dd7597ae42b2d886fa7
estefanomisme <3401@holbertonschool.com>
2021-08-13 20:54:41 +0000

    create file while it exists

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fl = open(filename, (O_EXCL | O_CREAT | O_TRUNC | O_WRONLY));
+	fl = open(filename, O_CREAT | O_APPEND | O_TRUNC | O_WRONLY,
+			S_IRUSR | S_IWUSR);
 	if (fl == -1)
 		return (-1);
 	if (text_content != NULL)
